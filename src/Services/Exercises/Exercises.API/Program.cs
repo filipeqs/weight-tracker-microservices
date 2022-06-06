d3b9c1ba0e64b39dd88f1209b87334c70e592883
Filipe Silva <filipeqs@outlook.com>
2022-06-05 21:53:51 -0400

    fix swagger doc name

--- src/Services/Exercises/Exercises.API/Program.cs
+++ src/Services/Exercises/Exercises.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Exercises.API", Version = "v1" });
 });
 
 builder.Services.AddScoped<IExerciseContext, ExerciseContext>();
